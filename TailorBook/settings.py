401796b201175417cdb053bb852c0b135f20ef4d
ordenkouton <143089996+Orden-Ktn@users.noreply.github.com>
2026-05-22 19:08:59 +0100

    update code

--- TailorBook/settings.py
+++ TailorBook/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-wkb0mrj75g8i!o*!d32q3r=+5s1%62=p#2rjnse%w$uhk#g0dr'
 
-DEBUG = True 
+DEBUG = False 
 
 ALLOWED_HOSTS = ['tailorbook.pythonanywhere.com', 'localhost', '127.0.0.1']
 
